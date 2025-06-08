8501a5b6e781a6220941b198ea6c0084b7102a1e
LauraR84 <laurarosano56@gmail.com>
2025-06-08 19:56:35 -0300

    Delete skyroute1.sql

--- skyroute1.sql
+++
@@ -1,7 +0,0 @@
-use skyroute;
-SELECT v.id_venta, c.razon_social, d.ciudad, d.pais, v.fecha_venta
-                FROM ventas v
-                JOIN clientes c ON v.id_cliente = c.id_cliente
-                JOIN destinos d ON v.id_destino = d.id_destino
-                WHERE v.estado = 'activo'
-                ORDER BY v.fecha_venta DESC;
